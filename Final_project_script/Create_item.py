8d89209931790ec70aad7359eb222b26ef10c8b7
Moradillocpe <qemoradillo@tip.edu.ph>
2026-04-15 15:54:41 +0800

    Modify file opening mode and update input prompts
    Changed file mode from 'w' to 'a' to append data. Updated input prompts to include colons for clarity.

--- Final_project_script/Create_item.py
+++ Final_project_script/Create_item.py
@@ -5,7 +5,7 @@ class Create_file():
         self.filename = filename
 
     def newfile(self):
-            with open(self.filename, "w", newline="") as my_file:
+            with open(self.filename, "a", newline="") as my_file:
                 write = csv.writer(my_file)
                 write.writerow(["Name", "components", "composition", "condition", "net_weight", "hazard", "recycle_rating"])
 
@@ -14,13 +14,13 @@ class Create_item(Create_file):
     def create_item(self):
         self.newfile()
 
-        self.Name = input("Input Item Name")
-        self.components = input("Input Item Component")
-        self.composition = input("Input Item Composition")
-        self.condition = input("Input Item Condition")
-        self.net_weight = input("Input Item Net Weight")
-        self.hazard = input("Input Item Hazard")
-        self.recycle_rating = input("Input Item Recycle Rating")
+        self.Name = input("Input Item Name: ")
+        self.components = input("Input Item Component: ")
+        self.composition = input("Input Item Composition: ")
+        self.condition = input("Input Item Condition: ")
+        self.net_weight = input("Input Item Net Weight: ")
+        self.hazard = input("Input Item Hazard: ")
+        self.recycle_rating = input("Input Item Recycle Rating: ")
 
         with open(self.filename, "a", newline="") as my_file:
             write = csv.writer(my_file)
