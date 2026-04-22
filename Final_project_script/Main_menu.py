cfa09f2b5a977617626b52a7d48f6d4643791ca9
Moradillocpe <qemoradillo@tip.edu.ph>
2026-04-22 23:07:00 +0800

    Integrate sorting feature into Main_menu
    Added sorting functionality to the main menu.

--- Final_project_script/Main_menu.py
+++ Final_project_script/Main_menu.py
@@ -1,6 +1,7 @@
 from refresh_list import Refresh_file
 from Create_item import Create_item
 from Update_item import Update_Item
+from sort_list import Sort_list
 
 class Main_menu():
     def __init__(self, user):
@@ -8,6 +9,7 @@ class Main_menu():
         self.Refresh_obj = Refresh_file()
         self.Create_Item_obj = Create_item()
         self.Update_obj = Update_Item()
+        self.Sort_obj = Sort_list()
         
     def show_menu(self):
         decision = 0
@@ -28,6 +30,7 @@ class Main_menu():
                 self.Refresh_obj.refresh_list()
             elif decision == 2:
                 print("=====Sort list=====")
+                self.Sort_obj.sort_list()
             elif decision == 3:
                 print("=====Create Item=====")
                 self.Create_Item_obj.create_item()
